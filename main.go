9487a7f9dff0a7be41180f27bd72976c809775e3
imorti <imorti@gmail.com>
2018-03-24 08:15:17 -0700

    now showing the actual paths for GOPATH and GOROOT.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	if gopath == "" {
 		colorstring.Println("[red] Uh oh, looks like GOPATH is not set.")
 	} else {
-		colorstring.Println("[green] Success! Well looks like GOPATH is configured properly.")
+		colorstring.Println("[green] Success! Well looks like GOPATH is configured properly. FYI it's here: " + gopath)
 	}
 
 	goroot := os.Getenv("GOROOT")
@@ -19,7 +19,7 @@ func main() {
 	if goroot == "" {
 		colorstring.Println("[red] Uh oh, Looks like GOROOT is not set.")
 	} else {
-		colorstring.Println("[green] Well looks like GOROOT is configured properly.")
+		colorstring.Println("[green] Well looks like GOROOT is configured properly. FYI it's here: " + goroot)
 	}
 
 }
